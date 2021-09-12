4430425312228a1013d0e344647c93aa965dbebb
nehavinod7d <88573323+nehavinod7d@users.noreply.github.com>
2021-09-12 16:05:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+var box1;
+var box2
+
+function setup() {
+  createCanvas(600, 400);
+  box1 = new Box();
+  box2 = new Box();
+  
+}
+
+function draw() {
+  background(220);
+  box1.show();
+  box1.set_width(100);
+  box2.show();
+  box2.set_width(200);
+}
+
